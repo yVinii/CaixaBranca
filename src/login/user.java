8a32e653d5bfd029c6ef40714ab4429a96b8ee30
yVinii <silveirafabrini@gmail.com>
2023-10-11 00:32:53 -0300

    first commit

--- src/login/user.java
+++ src/login/user.java
@@ -5,44 +5,64 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class user {
-    public Connection conectarBD(){ // 0
-        
-        Connection conn = null; // 1
-        
-        try{ // 2
-            Class.forName("com.mysql.Driver.Manager").newInstance();
-            String url = "jdbc:mysql//127.0.0.1/test?user=lopes&password=123";
-            conn = DriverManager.getConnection(url); 
-        
-        }catch(Exception e){ } // 3 e 4
-        
-        return conn;} // 5
-    
-    // 6 {
-    public String nome="";
-    public boolean result = false;
-    // } 6
-    
-    public boolean verificarUsuario(String login, String senha){ // 7 
-        
-        String sql=""; // 9 {
-        Connection conn = conectarBD();
-        //Instrução SQL
-        sql += "select nome from usuários";
-        sql += "where login  = " + "'" +login+"'";
-        sql += "and senha  = " + "'" +senha+"';";
-        // } 9
-        
-        try{ // 10
-            Statement st= conn.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            if(rs.next()){ // 15
-                result=true; // 16
-                nome = rs.getString("nome"); // 17
-            }}catch(Exception e){} // 11 e 12
-        return result; // 13 14
-        }
-    
-    }//fim da class
+/**
+ * Esta classe representa um usuário e fornece métodos para interagir com o banco de dados.
+ */
+public class User {
 
+    /**
+     * Método para ese conectar com o banco de dados.
+     * @return a conexão com o banco de dados ou null caso falhe.
+     */
+    public Connection conectarBD() {
+        Connection conn = null;
+
+        try {
+            // Carrega o MySQL e conecta com o banco de dados.
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
+            conn = DriverManager.getConnection(url);
+
+        } catch (Exception e) {
+        }
+
+        return conn;
+    }
+
+    // Variáveis de nome do usuário e o resultado da verificação.
+    public String nome = "";
+    public boolean result = false;
+
+    /**
+     * Com base no login e senha fornecidos verifica o usuário no banco de dados.
+     * @param login O nome de usuário.
+     * @param senha A senha do usuário.
+     * @return true se o usuário foi encontrado no banco de dados, false caso contrário.
+     */
+    public boolean verificarUsuario(String login, String senha) {
+        String sql = "";
+
+        // Conecta com o banco de dados.
+        Connection conn = conectarBD();
+
+        // Instrução SQL para consulta.
+        sql += "select nome from usuarios ";
+        sql += "where login = '" + login + "' ";
+        sql += "and senha = '" + senha + "';";
+
+        try {
+            // Objeto Statement e executa a consulta SQL.
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            if (rs.next()) {
+                // Caso o usuário foi encontrado no banco de dados.
+                result = true;
+                nome = rs.getString("nome");
+            }
+        } catch (Exception e) {
+        }
+
+        return result;
+    }
+}
